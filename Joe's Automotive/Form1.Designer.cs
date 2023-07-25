75bff4fb2a99ef6413f0e9e67211f65a2ab4c648
MHood09 <l.mickeyhood@gmail.com>
2023-07-25 16:34:11 -0500

    initial commit

---
+++ Joe's Automotive/Form1.Designer.cs
@@ -0,0 +1,390 @@
+﻿
+namespace Joe_s_Automotive
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lubeJobCheckBox = new System.Windows.Forms.CheckBox();
+            this.oilChangeCheckBox = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.transFlushCheckBox = new System.Windows.Forms.CheckBox();
+            this.radiatorFlushCheckBox = new System.Windows.Forms.CheckBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.tireRotationCheckBox = new System.Windows.Forms.CheckBox();
+            this.replaceMufflerCheckBox = new System.Windows.Forms.CheckBox();
+            this.inspectionCheckBox = new System.Windows.Forms.CheckBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.laborTextBox = new System.Windows.Forms.TextBox();
+            this.partsTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.totalFeesLabel = new System.Windows.Forms.Label();
+            this.taxLabel = new System.Windows.Forms.Label();
+            this.partsLabel = new System.Windows.Forms.Label();
+            this.serviceLaborLabel = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lubeJobCheckBox);
+            this.groupBox1.Controls.Add(this.oilChangeCheckBox);
+            this.groupBox1.Location = new System.Drawing.Point(74, 40);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(244, 99);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Oil and Lube";
+            // 
+            // lubeJobCheckBox
+            // 
+            this.lubeJobCheckBox.AutoSize = true;
+            this.lubeJobCheckBox.Location = new System.Drawing.Point(44, 65);
+            this.lubeJobCheckBox.Name = "lubeJobCheckBox";
+            this.lubeJobCheckBox.Size = new System.Drawing.Size(117, 19);
+            this.lubeJobCheckBox.TabIndex = 1;
+            this.lubeJobCheckBox.Text = "Lube Job ($18.00)";
+            this.lubeJobCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // oilChangeCheckBox
+            // 
+            this.oilChangeCheckBox.AutoSize = true;
+            this.oilChangeCheckBox.Location = new System.Drawing.Point(44, 31);
+            this.oilChangeCheckBox.Name = "oilChangeCheckBox";
+            this.oilChangeCheckBox.Size = new System.Drawing.Size(129, 19);
+            this.oilChangeCheckBox.TabIndex = 0;
+            this.oilChangeCheckBox.Text = "Oil Change ($26.00)";
+            this.oilChangeCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.transFlushCheckBox);
+            this.groupBox2.Controls.Add(this.radiatorFlushCheckBox);
+            this.groupBox2.Location = new System.Drawing.Point(412, 40);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(244, 99);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Flushes";
+            // 
+            // transFlushCheckBox
+            // 
+            this.transFlushCheckBox.AutoSize = true;
+            this.transFlushCheckBox.Location = new System.Drawing.Point(59, 65);
+            this.transFlushCheckBox.Name = "transFlushCheckBox";
+            this.transFlushCheckBox.Size = new System.Drawing.Size(169, 19);
+            this.transFlushCheckBox.TabIndex = 1;
+            this.transFlushCheckBox.Text = "Transmission Flush ($80.00)";
+            this.transFlushCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // radiatorFlushCheckBox
+            // 
+            this.radiatorFlushCheckBox.AutoSize = true;
+            this.radiatorFlushCheckBox.Location = new System.Drawing.Point(59, 31);
+            this.radiatorFlushCheckBox.Name = "radiatorFlushCheckBox";
+            this.radiatorFlushCheckBox.Size = new System.Drawing.Size(145, 19);
+            this.radiatorFlushCheckBox.TabIndex = 0;
+            this.radiatorFlushCheckBox.Text = "Radiator Flush ($30.00)";
+            this.radiatorFlushCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.tireRotationCheckBox);
+            this.groupBox3.Controls.Add(this.replaceMufflerCheckBox);
+            this.groupBox3.Controls.Add(this.inspectionCheckBox);
+            this.groupBox3.Location = new System.Drawing.Point(74, 159);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(243, 110);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Misc";
+            // 
+            // tireRotationCheckBox
+            // 
+            this.tireRotationCheckBox.AutoSize = true;
+            this.tireRotationCheckBox.Location = new System.Drawing.Point(44, 72);
+            this.tireRotationCheckBox.Name = "tireRotationCheckBox";
+            this.tireRotationCheckBox.Size = new System.Drawing.Size(137, 19);
+            this.tireRotationCheckBox.TabIndex = 2;
+            this.tireRotationCheckBox.Text = "Tire Rotation ($20.00)";
+            this.tireRotationCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // replaceMufflerCheckBox
+            // 
+            this.replaceMufflerCheckBox.AutoSize = true;
+            this.replaceMufflerCheckBox.Location = new System.Drawing.Point(44, 47);
+            this.replaceMufflerCheckBox.Name = "replaceMufflerCheckBox";
+            this.replaceMufflerCheckBox.Size = new System.Drawing.Size(159, 19);
+            this.replaceMufflerCheckBox.TabIndex = 1;
+            this.replaceMufflerCheckBox.Text = "Replace Muffler ($100.00)";
+            this.replaceMufflerCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // inspectionCheckBox
+            // 
+            this.inspectionCheckBox.AutoSize = true;
+            this.inspectionCheckBox.Location = new System.Drawing.Point(44, 22);
+            this.inspectionCheckBox.Name = "inspectionCheckBox";
+            this.inspectionCheckBox.Size = new System.Drawing.Size(125, 19);
+            this.inspectionCheckBox.TabIndex = 0;
+            this.inspectionCheckBox.Text = "Inspection ($15.00)";
+            this.inspectionCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.laborTextBox);
+            this.groupBox4.Controls.Add(this.partsTextBox);
+            this.groupBox4.Controls.Add(this.label2);
+            this.groupBox4.Controls.Add(this.label1);
+            this.groupBox4.Location = new System.Drawing.Point(412, 159);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(243, 109);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Parts and Labor";
+            // 
+            // laborTextBox
+            // 
+            this.laborTextBox.Location = new System.Drawing.Point(126, 69);
+            this.laborTextBox.Name = "laborTextBox";
+            this.laborTextBox.Size = new System.Drawing.Size(69, 23);
+            this.laborTextBox.TabIndex = 3;
+            // 
+            // partsTextBox
+            // 
+            this.partsTextBox.Location = new System.Drawing.Point(126, 34);
+            this.partsTextBox.Name = "partsTextBox";
+            this.partsTextBox.Size = new System.Drawing.Size(69, 23);
+            this.partsTextBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Labor ($)";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(80, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Parts";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.totalFeesLabel);
+            this.groupBox5.Controls.Add(this.taxLabel);
+            this.groupBox5.Controls.Add(this.partsLabel);
+            this.groupBox5.Controls.Add(this.serviceLaborLabel);
+            this.groupBox5.Controls.Add(this.label6);
+            this.groupBox5.Controls.Add(this.label5);
+            this.groupBox5.Controls.Add(this.label4);
+            this.groupBox5.Controls.Add(this.label3);
+            this.groupBox5.Location = new System.Drawing.Point(74, 283);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(580, 170);
+            this.groupBox5.TabIndex = 4;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Summary";
+            // 
+            // totalFeesLabel
+            // 
+            this.totalFeesLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.totalFeesLabel.Location = new System.Drawing.Point(188, 130);
+            this.totalFeesLabel.Name = "totalFeesLabel";
+            this.totalFeesLabel.Size = new System.Drawing.Size(118, 23);
+            this.totalFeesLabel.TabIndex = 7;
+            // 
+            // taxLabel
+            // 
+            this.taxLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.taxLabel.Location = new System.Drawing.Point(188, 95);
+            this.taxLabel.Name = "taxLabel";
+            this.taxLabel.Size = new System.Drawing.Size(118, 23);
+            this.taxLabel.TabIndex = 6;
+            // 
+            // partsLabel
+            // 
+            this.partsLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.partsLabel.Location = new System.Drawing.Point(188, 64);
+            this.partsLabel.Name = "partsLabel";
+            this.partsLabel.Size = new System.Drawing.Size(118, 23);
+            this.partsLabel.TabIndex = 5;
+            // 
+            // serviceLaborLabel
+            // 
+            this.serviceLaborLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.serviceLaborLabel.Location = new System.Drawing.Point(188, 29);
+            this.serviceLaborLabel.Name = "serviceLaborLabel";
+            this.serviceLaborLabel.Size = new System.Drawing.Size(118, 23);
+            this.serviceLaborLabel.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(86, 130);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(58, 15);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Total Fees";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(66, 95);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 15);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Tax (on parts)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(111, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 15);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Parts";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 15);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Service and Labor";
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(118, 467);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(103, 32);
+            this.calculateButton.TabIndex = 5;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(308, 467);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(99, 32);
+            this.clearButton.TabIndex = 6;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(515, 467);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(101, 32);
+            this.exitButton.TabIndex = 7;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(716, 550);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Automotive";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox lubeJobCheckBox;
+        private System.Windows.Forms.CheckBox oilChangeCheckBox;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox transFlushCheckBox;
+        private System.Windows.Forms.CheckBox radiatorFlushCheckBox;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.CheckBox tireRotationCheckBox;
+        private System.Windows.Forms.CheckBox replaceMufflerCheckBox;
+        private System.Windows.Forms.CheckBox inspectionCheckBox;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox laborTextBox;
+        private System.Windows.Forms.TextBox partsTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Label totalFeesLabel;
+        private System.Windows.Forms.Label taxLabel;
+        private System.Windows.Forms.Label partsLabel;
+        private System.Windows.Forms.Label serviceLaborLabel;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Joe's Automotive/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Joe_s_Automotive
+{
+    public partial class Form1 : Form
+    {
+        public object Resquest { get; private set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public decimal oilLubeCharges()
+        {
+            decimal Oil;
+            decimal Lube;
+            if(oilChangeCheckBox.Checked == true)
+            {
+                Oil = 26.00m;
+           
+            }
+            else
+            {
+                Oil = 0;
+            }
+            if(lubeJobCheckBox.Checked == true)
+            {
+                Lube = 18.00m;
+            }
+            else
+            {
+                Lube = 0;
+            }
+
+            return Oil + Lube;
+        }
+
+        public decimal flushCharges()
+        {
+            decimal Radiator;
+            decimal Trans;
+            if (radiatorFlushCheckBox.Checked == true)
+            {
+                Radiator = 30.00m;
+            }
+            else
+            {
+                Radiator = 0;
+            }
+            if (transFlushCheckBox.Checked == true)
+            {
+                Trans = 80.00m;
+            }
+            else
+            {
+                Trans = 0;
+            }
+
+            return Radiator + Trans;
+        }
+
+        public decimal miscCharges()
+        {
+            decimal Tire;
+            decimal Muffler;
+            decimal Inspection;
+            if (tireRotationCheckBox.Checked == true)
+            {
+                Tire = 20.00m;
+            }
+            else
+            {
+                Tire = 0;
+            }
+            if (replaceMufflerCheckBox.Checked == true)
+            {
+                Muffler = 100.00m;
+            }
+            else
+            {
+                Muffler = 0;
+            }
+            if (inspectionCheckBox.Checked == true)
+            {
+                Inspection = 15.00m;
+            }
+            else
+            {
+                Inspection = 0;
+            }
+            
+            return Tire + Muffler + Inspection;
+        }
+
+        public void oilLubeClear()
+        {
+            oilChangeCheckBox.Checked = false;
+            lubeJobCheckBox.Checked = false;
+        }
+
+        public void miscClear()
+        {
+            inspectionCheckBox.Checked = false;
+            replaceMufflerCheckBox.Checked = false;
+            tireRotationCheckBox.Checked = false;
+        }
+
+        public void flushesClear()
+        {
+            radiatorFlushCheckBox.Checked = false;
+            transFlushCheckBox.Checked = false;
+        }
+
+        public void partsLaborClear()
+        {
+            partsTextBox.Text = "";
+            laborTextBox.Text = "";
+        }
+
+        public void summaryClear()
+        {
+            serviceLaborLabel.Text = "";
+            partsLabel.Text = "";
+            taxLabel.Text = "";
+            totalFeesLabel.Text = "";
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            oilLubeClear();
+            miscClear();
+            flushesClear();
+            partsLaborClear();
+            summaryClear();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        {
+            decimal Parts = 0;
+            decimal Labor = 0;
+            decimal Tax = 0;
+            decimal Total = 0;
+            if (string.IsNullOrEmpty(partsTextBox.Text))
+            {
+                partsLabel.Text = 0.ToString("c");
+                taxLabel.Text = 0.ToString("c");
+            }
+            else
+                if (decimal.TryParse(partsTextBox.Text, out Parts))
+                {
+                    Tax = Parts * .06m;
+                    partsLabel.Text = Parts.ToString("c");
+                    taxLabel.Text = Tax.ToString("c");
+                }
+            else
+            {
+                MessageBox.Show("Parts must be numeric");
+            }
+
+            if (string.IsNullOrEmpty(laborTextBox.Text))
+            {
+                serviceLaborLabel.Text = 0.ToString("c");
+            }
+            else 
+                if (decimal.TryParse(laborTextBox.Text, out Labor))
+                {
+                    serviceLaborLabel.Text = Labor.ToString("c");
+                }
+            else
+            {
+                MessageBox.Show("Labor must be numeric");
+            }
+
+            Total = oilLubeCharges() + flushCharges() + miscCharges() + Labor + Parts + Tax;
+
+            totalFeesLabel.Text = Total.ToString("c");
+
+        }
+    }
+}
